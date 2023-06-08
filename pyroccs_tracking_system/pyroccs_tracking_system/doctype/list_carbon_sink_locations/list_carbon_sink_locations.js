bea03821091725ff008a9c50830426662f2f6b13
jaysarna <sarnajay@gmail.com>
2023-06-08 12:30:07 +0500

    carbon standards

---
+++ pyroccs_tracking_system/pyroccs_tracking_system/doctype/list_carbon_sink_locations/list_carbon_sink_locations.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, Deepak Kumar and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('List Carbon Sink Locations', {
+	// refresh: function(frm) {
+
+	// }
+});
