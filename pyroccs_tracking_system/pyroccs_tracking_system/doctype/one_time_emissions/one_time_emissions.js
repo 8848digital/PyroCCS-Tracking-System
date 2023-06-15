ad7d44862c8765fe7b731be2ef476c018b12c2df
Deepak Kumar <deepakkumar@8848digital.com>
2023-06-15 15:07:05 +0530

    change one time emission to child table

--- pyroccs_tracking_system/pyroccs_tracking_system/doctype/one_time_emissions/one_time_emissions.js
+++ pyroccs_tracking_system/pyroccs_tracking_system/doctype/one_time_emissions/one_time_emissions.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2023, Deepak Kumar and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('List of One-Time Emissions', {
+frappe.ui.form.on('One-Time Emissions', {
 	// refresh: function(frm) {
 
 	// }
