d66e56aabdfd5517a50eeeee7b2214d2bb436412
Deepak Kumar <deepakkumar@8848digital.com>
2023-06-15 17:27:57 +0530

    change doctype names

--- pyroccs_tracking_system/pyroccs_tracking_system/doctype/list_of_plant_owners/list_of_plant_owners.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) 2023, Deepak Kumar and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-class ListofPlantOwners(Document):
-	pass
