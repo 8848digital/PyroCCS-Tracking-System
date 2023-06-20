de648d9de1e9a431b74b747aee08171b1d58e661
Deepak Kumar <deepakkumar@8848digital.com>
2023-06-20 14:18:35 +0530

    biochar lab address

--- pyroccs_tracking_system/pyroccs_tracking_system/doctype/biochar_labs/biochar_labs.js
+++ pyroccs_tracking_system/pyroccs_tracking_system/doctype/biochar_labs/biochar_labs.js
@@ -2,7 +2,37 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Biochar Labs', {
-	// refresh: function(frm) {
+	setup: function(frm) {
+		frm.set_query("address", function(doc) {
+			return {
+				filters: {
+					"link_doctype": doc.doctype,
+					"link_name": doc.name
+				}
+			};
+		});
+	},
+	
+	refresh: function(frm) {
+		frappe.dynamic_link = { doc: frm.doc, fieldname: 'name', doctype: frm.doctype }
+		unhide_field(['address_html','contact_html']);
+		frappe.contacts.render_address_and_contact(frm);
+	},
 
-	// }
+	// address: function(frm) {
+	// 	if (frm.doc.address) {
+	// 		frappe.call({
+	// 			method: 'frappe.contacts.doctype.address.address.get_address_display',
+	// 			args: {
+	// 				"address_dict": frm.doc.supplier_primary_address
+	// 			},
+	// 			callback: function(r) {
+	// 				frm.set_value("primary_address", r.message);
+	// 			}
+	// 		});
+	// 	}
+	// 	if (!frm.doc.supplier_primary_address) {
+	// 		frm.set_value("primary_address", "");
+	// 	}
+	// },
 });
--- pyroccs_tracking_system/pyroccs_tracking_system/doctype/biochar_labs/biochar_labs.py
+++ pyroccs_tracking_system/pyroccs_tracking_system/doctype/biochar_labs/biochar_labs.py
@@ -1,8 +1,25 @@
 # Copyright (c) 2023, Deepak Kumar and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
+from frappe.contacts.address_and_contact import (
+	delete_contact_and_address,
+	load_address_and_contact,
+)
+
 
 class BiocharLabs(Document):
-	pass
+	
+
+	def onload(self):
+		"""Load address and contacts in `__onload`"""
+		load_address_and_contact(self)
+
+	def on_trash(self):
+		if self.contact:
+			self.db_set("contact", None)
+		if self.address:
+			self.db_set("address", None)
+
+		delete_contact_and_address(self.doctype, self.name)
