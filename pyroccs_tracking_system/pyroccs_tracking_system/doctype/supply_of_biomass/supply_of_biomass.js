c260c18770de5c6bb9d1d978f5e627c89a71cfc1
Deepak Kumar <deepakkumar@8848digital.com>
2023-06-20 18:36:22 +0530

    distance to plant hide

--- pyroccs_tracking_system/pyroccs_tracking_system/doctype/supply_of_biomass/supply_of_biomass.js
+++ pyroccs_tracking_system/pyroccs_tracking_system/doctype/supply_of_biomass/supply_of_biomass.js
@@ -3,14 +3,22 @@
 
 frappe.ui.form.on('Supply of Biomass', {
 	refresh: function(frm) {
-		console.log("Script Load")
+		if (frm.doc.biomass_supply==="Select Source") {
+			frm.set_df_property("supply_source","hidden", 0)
+			frm.set_df_property("distance_to_plant","hidden", 0)
+		} else {
+			frm.set_df_property("supply_source","hidden", 1) 
+			frm.set_df_property("distance_to_plant","hidden", 1)
+		}
 	},
 	biomass_supply: function(frm) {
 	// show supply source if "Select Source"
 		if (frm.doc.biomass_supply==="Select Source") {
 			frm.set_df_property("supply_source","hidden", 0)
+			frm.set_df_property("distance_to_plant","hidden", 0)
 		} else {
 			frm.set_df_property("supply_source","hidden", 1) 
+			frm.set_df_property("distance_to_plant","hidden", 1)
 		}
 	}
 });
